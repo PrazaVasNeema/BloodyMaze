d2b6e533f8f0e4a6eb1e93aa281ddc1cedbe1155
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-02-24 17:09:18 +0300

    Стрельба

--- Assets/Scripts/Components/UseAbilityComponentModuleRevolver.cs
+++ Assets/Scripts/Components/UseAbilityComponentModuleRevolver.cs
@@ -7,16 +7,25 @@ namespace BloodyMaze.Components
     public class UseAbilityComponentModuleRevolver : UseAbilityComponentModuleAbstract
     {
         [SerializeField] private string m_ammoTypeName;
+        [SerializeField] private float m_shootForce;
         private AmmunitionComponent m_ammunitionComponent;
+        private IAbilityComponent m_abilityComponent;
+
 
         private void Awake()
         {
             m_ammunitionComponent = transform.parent.transform.parent.GetComponent<AmmunitionComponent>();
+            m_abilityComponent = GetComponent<IAbilityComponent>();
         }
 
         public override bool Check()
         {
-            return m_ammunitionComponent.ShootAmmo(m_ammoTypeName);
+            bool canAttack = m_ammunitionComponent.ShootAmmo(m_ammoTypeName);
+            if (canAttack)
+            {
+                m_abilityComponent.UseAbility(m_shootForce);
+            }
+            return canAttack;
         }
     }
 }
