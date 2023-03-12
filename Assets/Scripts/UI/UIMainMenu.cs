3ece542334d4089ae8b780dba2e76abdc3ab6d7a
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-12 23:49:01 +0300

    Refactor UI, 1/4

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BloodyMaze.UI
+{
+    [RequireComponent(typeof(Animator))]
+    public class UIMainMenu : UIPanelBaseAbstract
+    {
+        public enum MenuStates { INTRO, MAIN };
+
+        private Animator m_animator;
+        private MenuStates m_state = MenuStates.INTRO;
+        public MenuStates state => m_state;
+
+        private void Awake()
+        {
+            TryGetComponent(out m_animator);
+        }
+
+        public void ChangeState()
+        {
+            switch (m_state)
+            {
+                case MenuStates.INTRO:
+                    m_state = MenuStates.MAIN;
+                    m_animator.SetBool("AppearAndDisappear", false);
+                    m_animator.SetTrigger("ZoomIn");
+                    break;
+                case MenuStates.MAIN:
+                    m_state = MenuStates.INTRO;
+                    m_animator.SetBool("AppearAndDisappear", true);
+                    m_animator.SetTrigger("ZoomOut");
+                    break;
+            }
+        }
+
+        public void OnEnterGame()
+        {
+            UICore.current.LoadLevel("SampleScene");
+        }
+
+        public void OnExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
