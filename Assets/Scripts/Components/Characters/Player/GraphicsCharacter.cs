bd41862c93c02de79b271360db2ef0e28c180ba6
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-21 19:18:20 +0300

    Added rotations during targetting, fixed backwards targeting walk

--- Assets/Scripts/Components/Characters/Player/GraphicsCharacter.cs
+++ Assets/Scripts/Components/Characters/Player/GraphicsCharacter.cs
@@ -17,10 +17,12 @@ namespace BloodyMaze.Components
 
         private Vector3 m_lastPosition;
         private GameObject m_currentTarget;
+        private bool m_isTurnedAround;
 
-
+        private Quaternion m_manSpineErrorRoot;
         private void Awake()
         {
+            m_manSpineErrorRoot = m_manSpine.rotation;
             m_animator = GetComponent<Animator>();
             m_abilitiesManager = GetComponentInParent<AbilitiesManager>();
             m_movementComponentCharacter = GetComponentInParent<MovementComponentCharacter>();
@@ -44,6 +46,8 @@ namespace BloodyMaze.Components
             if (m_currentTarget == null)
             {
                 m_manSpine.LookAt(gameObject.transform.forward);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                m_isTurnedAround = false;
             }
         }
 
@@ -56,13 +60,29 @@ namespace BloodyMaze.Components
         private void LateUpdate()
         {
             // transform.LookAt(new Vector3(1, transform.position.y, 1));
-
+            // transform.LookAt(m_currentTarget.transform);
             var velocity = Mathf.Clamp01(m_movementComponentCharacter.velocity / m_movementComponentCharacter.speed);
             m_animator.SetFloat(SpeedMoveId, velocity);
             // m_manSpine.LookAt(Vector3.Normalize(new Vector3(gameObject.transform.forward.x, gameObject.transform.forward.y, gameObject.transform.forward.z)));
             if (m_currentTarget)
             {
-                m_manSpine.LookAt(new Vector3(m_currentTarget.transform.position.x, m_manSpine.transform.position.y, m_currentTarget.transform.position.z));
+                var m_currentTargetPosition = m_currentTarget.transform.position;
+                var dir = new Vector3(m_currentTargetPosition.x, m_manSpine.position.y, m_currentTargetPosition.z) - m_manSpine.position;
+                var rotation = Quaternion.LookRotation(dir) * m_manSpineErrorRoot;
+                m_manSpine.rotation = rotation;
+
+                Vector3 PCForward = transform.forward, manSpineForward = m_manSpine.transform.forward;
+                var dotProduct = PCForward.x * manSpineForward.x + PCForward.y * manSpineForward.y + PCForward.z * manSpineForward.z;
+                var angle = Mathf.Acos(dotProduct / (PCForward.sqrMagnitude * manSpineForward.sqrMagnitude));
+                // Debug.Log($"Angle: {angle}, {75f * Mathf.Deg2Rad}");
+                if (angle > 1.57f)
+                {
+                    transform.localRotation = Quaternion.Euler(0, m_isTurnedAround ? 0 : 180, 0);
+                    // transform.localScale = new Vector3(1, 1, -transform.localScale.z);
+                    m_isTurnedAround = !m_isTurnedAround;
+                }
+
+                // m_manSpine.LookAt(new Vector3(m_currentTarget.transform.position.x, m_manSpine.transform.position.y, m_currentTarget.transform.position.z));
                 //     // m_manSpine.rotation = new Quaternion(m_manSpine.rotation.x, 0, m_manSpine.rotation.z, 0);
                 //     // m_manSpine.forward = Vector3.Normalize(new Vector3(m_currentTarget.transform.position.x - m_manSpine.transform.position.x, 1, m_currentTarget.transform.position.x - m_manSpine.transform.position.x));
                 //     var lookPos = m_currentTarget.transform.position - m_manSpine.transform.position;
@@ -73,6 +93,11 @@ namespace BloodyMaze.Components
                 //     m_manSpine.transform.rotation = rotation;
             }
             // m_manSpine.forward = m_currentTarget.transform.position - m_manSpine.transform.position;
+
+        }
+
+        private class SubForCheckingAngle
+        {
 
         }
     }
