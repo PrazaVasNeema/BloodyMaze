f9d5935302099e39eb3b4ff61d797b77a4d6d96a
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-22 04:56:40 +0300

    Placed some new notes

--- Assets/Scripts/Audio/ActivateModulePlaySound.cs
+++ Assets/Scripts/Audio/ActivateModulePlaySound.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 namespace BloodyMaze.Components
 {
+    [RequireComponent(typeof(AudioSource))]
     public class ActivateModulePlaySound : ActivateModuleAbstract
     {
-        [SerializeField] private List<AudioSource> m_audioSources;
+        [SerializeField] private List<AudioClip> m_audioClips;
         [SerializeField] private bool m_shouldPlayRandom;
         private int index = 0;
+        private AudioSource m_audioSource;
+
+        private void Awake()
+        {
+            m_audioSource = GetComponent<AudioSource>();
+            m_audioSource.playOnAwake = false;
+        }
 
         public override void ActivateModule()
         {
-            if (m_audioSources.Count > 0)
+            if (m_audioClips.Count > 0)
             {
-                index = m_shouldPlayRandom ? Random.Range(0, m_audioSources.Count) : (m_audioSources.Count + ++index) % m_audioSources.Count;
-                m_audioSources[index].Play();
+                index = m_shouldPlayRandom ? Random.Range(0, m_audioClips.Count) : (m_audioClips.Count + ++index) % m_audioClips.Count;
+                m_audioSource.clip = m_audioClips[index];
+                m_audioSource.Play();
             }
         }
     }
