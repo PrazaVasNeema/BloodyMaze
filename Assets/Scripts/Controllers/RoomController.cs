a8150cce71d0ac5b19a2d40ad3b91c5182153f5d
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-23 21:50:23 +0300

    PlaySound for readNote

--- Assets/Scripts/Controllers/RoomController.cs
+++ Assets/Scripts/Controllers/RoomController.cs
@@ -26,6 +26,7 @@ namespace BloodyMaze.Controllers
             for (int i = 0; i < m_roomActivaters.Length; i++)
             {
                 bool flag = true;
+                Debug.Log(roomID);
                 foreach (string str in m_roomActivaters[i].eventsShouldBeChecked)
                 {
                     var globalEventsEvent = m_globalEventsData.Find((x) => x.eventKey == str);
