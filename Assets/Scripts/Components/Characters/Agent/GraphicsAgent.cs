2813113fb780612a40b590635a45900c3a34985f
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-24 11:34:17 +0300

    Ghost fly and idle animations

--- Assets/Scripts/Components/Characters/Agent/GraphicsAgent.cs
+++ Assets/Scripts/Components/Characters/Agent/GraphicsAgent.cs
@@ -6,12 +6,16 @@ namespace BloodyMaze.Components
 {
     public class GraphicsAgent : MonoBehaviour
     {
-        private Animator m_animator;
+        [SerializeField] private Animator m_animator;
         [SerializeField] private Animator m_animatorLocked;
+        private Vector3 m_prevPosition;
+        private float m_moveSpeed;
+        protected float blendIdleFlyChangingSpeed = 0f;
+
 
         private void Awake()
         {
-            m_animator = GetComponent<Animator>();
+            // m_animator = GetComponent<Animator>();
             // m_animatorLocked = GetComponentsInChildren<Animator>()[1];
         }
 
@@ -29,5 +33,17 @@ namespace BloodyMaze.Components
         {
             m_animatorLocked.SetBool("SetTargetIsBeingLocked", flag);
         }
+
+        private void LateUpdate()
+        {
+            Debug.Log(Vector3.SqrMagnitude(transform.position - m_prevPosition));
+            float moveSpeedDir = Vector3.SqrMagnitude(transform.position - m_prevPosition) > 0f ? 1f : 0f;
+            // m_moveSpeed = Mathf.Lerp(m_moveSpeed, moveSpeedDir, Time.deltaTime);
+            m_moveSpeed = Mathf.SmoothDamp(m_moveSpeed, moveSpeedDir, ref blendIdleFlyChangingSpeed, 0.5f, 1f);
+            m_animator.SetFloat("MoveSpeed", m_moveSpeed);
+            m_prevPosition = transform.position;
+
+        }
+
     }
 }
