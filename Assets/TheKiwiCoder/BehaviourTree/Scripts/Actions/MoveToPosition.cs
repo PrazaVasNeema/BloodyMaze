f192151c01030886b4153b4c6ee0e11afdfaad4b
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-25 20:47:40 +0300

    1

--- Assets/TheKiwiCoder/BehaviourTree/Scripts/Actions/MoveToPosition.cs
+++ Assets/TheKiwiCoder/BehaviourTree/Scripts/Actions/MoveToPosition.cs
@@ -6,35 +6,36 @@ using TheKiwiCoder;
 [System.Serializable]
 public class MoveToPosition : ActionNode
 {
-	public float stoppingDistance = 0.1f;
+    public float stoppingDistance = 0.1f;
 
-	protected override void OnStart()
-	{
-		context.agent.stoppingDistance = stoppingDistance;
-		context.agent.destination = blackboard.moveToPosition;
-	}
+    protected override void OnStart()
+    {
+        context.agent.stoppingDistance = stoppingDistance;
+        context.agent.destination = blackboard.moveToPosition;
+        Debug.Log(context.agent.destination);
+    }
 
-	protected override void OnStop()
-	{
-	}
+    protected override void OnStop()
+    {
+    }
 
-	protected override State OnUpdate()
-	{
-		if (context.agent.pathPending)
-		{
-			return State.Running;
-		}
+    protected override State OnUpdate()
+    {
+        if (context.agent.pathPending)
+        {
+            return State.Running;
+        }
 
-		if (context.agent.remainingDistance < stoppingDistance)
-		{
-			return State.Success;
-		}
+        if (context.agent.remainingDistance < stoppingDistance)
+        {
+            return State.Success;
+        }
 
-		if (context.agent.pathStatus == UnityEngine.AI.NavMeshPathStatus.PathInvalid)
-		{
-			return State.Failure;
-		}
+        if (context.agent.pathStatus == UnityEngine.AI.NavMeshPathStatus.PathInvalid)
+        {
+            return State.Failure;
+        }
 
-		return State.Running;
-	}
+        return State.Running;
+    }
 }
