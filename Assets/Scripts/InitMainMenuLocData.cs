8e867878945a0e7cfe9fdbd2dbe0abcd21364649
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-03-26 14:53:13 +0300

    Loc interface main menu

--- Assets/Scripts/InitMainMenuLocData.cs
+++ Assets/Scripts/InitMainMenuLocData.cs
@@ -16,6 +16,7 @@ namespace BloodyMaze
         [SerializeField] private TMP_Text optionsBack;
         [SerializeField] private TMP_Text optionsApply;
         [SerializeField] private TMP_Text levelSelectBack;
+        [SerializeField] private TMP_Text levelSelectSelectLevel;
         [SerializeField] private TMP_Text levelSelectLoad;
         [SerializeField] private TMP_Text levelSelectNew;
         [SerializeField] private TMP_Text saveSlot1_1;
@@ -24,6 +25,7 @@ namespace BloodyMaze
         [SerializeField] private TMP_Text saveSlot2_2;
         [SerializeField] private TMP_Text saveSlot3_1;
         [SerializeField] private TMP_Text saveSlot3_2;
+        [SerializeField] private TMP_Text[] backButtons;
 
         [SerializeField] private string mainButtonPlayKey;
         [SerializeField] private string mainOptionsKey;
@@ -42,6 +44,7 @@ namespace BloodyMaze
         [SerializeField] private string saveSlot2_2Key;
         [SerializeField] private string saveSlot3_1Key;
         [SerializeField] private string saveSlot3_2Key;
+        [SerializeField] private string levelSelectSelectLevelKey;
 
         public void SetLocDataMainMenu()
         {
@@ -58,6 +61,16 @@ namespace BloodyMaze
             levelSelectBack.text = GameController.locData.GetInterfaceText(levelSelectBackKey);
             levelSelectLoad.text = GameController.locData.GetInterfaceText(levelSelectLoadKey);
             levelSelectNew.text = GameController.locData.GetInterfaceText(levelSelectNewKey);
+            levelSelectSelectLevel.text = GameController.locData.GetInterfaceText(levelSelectSelectLevelKey);
+
+            foreach (TMP_Text backButton in backButtons)
+            {
+                backButton.text = GameController.locData.GetInterfaceText(levelSelectBackKey);
+            }
+
+            saveSlot1_1.text = $"{GameController.locData.GetInterfaceText(saveSlot1_1Key)} + 1";
+            saveSlot2_1.text = $"{GameController.locData.GetInterfaceText(saveSlot1_1Key)} + 2";
+            saveSlot3_1.text = $"{GameController.locData.GetInterfaceText(saveSlot1_1Key)} + 3";
         }
     }
 }
