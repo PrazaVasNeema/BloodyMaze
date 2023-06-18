367250270815da5d613e46b580df0b5dcb54ef17
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2023-06-18 17:10:15 +0300

    New optimized interface loc system 2

--- Assets/Scripts/UI/UIChooseSaveFile.cs
+++ Assets/Scripts/UI/UIChooseSaveFile.cs
@@ -14,6 +14,9 @@ namespace BloodyMaze.UI
         {
             for (int i = 0; i < 3; i++)
             {
+                TMP_Text curSaveSlotText = m_saveSlotsLayoutGroup.transform.GetChild(i).GetChild(0).GetComponent<TMP_Text>();
+                if (!curSaveSlotText.text.Contains((i + 1).ToString()))
+                    curSaveSlotText.text += $" {i + 1}";
                 PlayerProfileData playerProfileData = GameController.instance.allPlayerProfilesData[i];
                 int progressPercent = new();
                 int totalEventsTrue = 0;
@@ -23,7 +26,7 @@ namespace BloodyMaze.UI
                     playerProfileData.globalEventsData.ForEach(x => totalEventsTrue += x.flag == true ? 1 : 0);
                 }
                 m_saveSlotsLayoutGroup.transform.GetChild(i).GetChild(1).GetComponent<TMP_Text>().text = playerProfileData
-                == null ? "Пустой слот" : $"Прогресс: {Mathf.Round(totalEventsTrue * 1f / (progressPercent * 1f) * 100f * 1f)}%";
+                == null ? GameController.instance.locData.GetInterfaceText("UILoc_save_file_progress_empty") : $"{GameController.instance.locData.GetInterfaceText("UILoc_save_file_progress")}: {Mathf.Round(totalEventsTrue * 1f / (progressPercent * 1f) * 100f * 1f)}%";
                 if (GameController.instance.shouldStartNewGame == false && playerProfileData == null)
                     m_saveSlotsLayoutGroup.transform.GetChild(i).GetComponent<Button>().interactable = false;
                 else
